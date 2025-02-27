54329b5585f655ba76dd89ca4c0ccb06fdd8e3f9
yueyinqiu <yueyinqiu@outlook.com>
2025-02-27 23:34:57 +0800

    ric

---
+++ src/exe/ricbe/ric/train_ricbe_ric_config.py
@@ -0,0 +1,29 @@
+import pathlib as _pathlib
+import torch as _torch
+
+from exe import common_configurations as _common_configurations
+from exe.data.preprocess import split_dataset_config as _split_dataset_config
+
+
+device: _torch.device = \
+    _common_configurations.device
+
+
+checkpoint_interval: int = \
+    1000
+
+
+checkpoints_directory: _pathlib.Path = \
+    _common_configurations.checkpoints_directory / "ricbe_ric/"
+
+
+train_list_rir: _pathlib.Path = \
+    _split_dataset_config.train_list_rir
+
+
+train_list_speech: _pathlib.Path = \
+    _split_dataset_config.train_list_speech
+
+
+random_seed: str = \
+    "F8D4DAED-AF3A-42B5-AA3D-57C44C114150"
