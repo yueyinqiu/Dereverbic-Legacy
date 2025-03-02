7662c1935e808ead09c7169d9e88bf9ab94aee78
yueyinqiu <yueyinqiu@outlook.com>
2025-03-02 14:01:37 +0800

    ricmrstftonly

---
+++ src/exe/ricbe/ric_mrstft_only/test_ricbe_ric_mrstft_only.py
@@ -0,0 +1,92 @@
+import csv
+from pathlib import Path
+from random import Random
+import sys
+from typing import Callable
+import csfile
+from statictorch import Tensor1d, Tensor2d
+import torch
+from torch.utils.data import DataLoader
+
+from audio_processors.rir_acoustic_features import RirAcousticFeatures
+from basic_utilities.kahan_accumulator import KahanAccumulator
+from inputs_and_outputs.checkpoint_managers.checkpoints_directory import CheckpointsDirectory
+from inputs_and_outputs.checkpoint_managers.epoch_and_path import EpochAndPath
+from inputs_and_outputs.csv_accessors.csv_writer import CsvWriter
+from inputs_and_outputs.data_providers.data_batch import DataBatch
+from inputs_and_outputs.data_providers.validation_or_test_dataset import ValidationOrTestDataset
+from metrics.bias_metric import BiasMetric
+from metrics.l1_loss_metric import L1LossMetric
+from metrics.metric import Metric
+from metrics.mrstft_loss_metric import MrstftLossMetric
+from metrics.pearson_correlation_metric import PearsonCorrelationMetric
+from metrics.rir_reverberation_time_metrics import RirReverberationTimeMetrics
+from models.cleanunet_models.cleanunet_model import CleanunetModel
+from models.fins_models.fins_model import FinsModel
+from models.ricbe_models.ricbe_ric_model import RicbeRicModel
+from models.ricbe_models.ricbe_ric_model_mrstft_only import RicbeRicModelMrstftOnly
+from trainers.trainer import Trainer
+
+
+def test(model: RicbeRicModelMrstftOnly, 
+         checkpoints: CheckpointsDirectory, 
+         data: DataLoader, 
+         metrics: dict[str, Metric[Tensor2d]]):
+    with torch.no_grad():
+        print(f"# Batch count: {data.__len__()}")
+
+        rank_file: Path = checkpoints.get_path(None) / "validation_rank.txt"
+        if rank_file.exists():
+            epoch: int = int(csfile.read_all_lines(rank_file)[0])
+            path: Path = checkpoints.get_path(epoch)
+            print(f"# Rank file found. The best checkpoint {epoch} will be used.")
+        else:
+            latest: EpochAndPath | None = checkpoints.get_latest()
+            if not latest:
+                raise FileNotFoundError("Failed to find any checkpoint in the checkpoints directory.")
+            epoch, path = latest
+            print(f"# Failed to find the rank file. The latest checkpoint {epoch} will be used.")
+
+        csv_print: CsvWriter = csv.writer(sys.stdout)
+        csv_print.writerow(["batch", "metric", "submetric", "value"])
+
+        Trainer.load_model(model, path)
+
+        batch_index: int
+        batch: DataBatch
+        for batch_index, batch in enumerate(data):
+            predicted: Tensor2d = model.evaluate_on(batch.reverb, batch.speech)
+
+            metric: str
+            for metric in metrics:
+                current: dict[str, float] = metrics[metric].append(batch.rir, predicted)
+                submetric: str
+                for submetric in current:
+                    csv_print.writerow([batch_index, metric, submetric, current[submetric]])
+
+        for metric in metrics:
+            value: float
+            for submetric, value in metrics[metric].result().items():
+                csv_print.writerow(["all", metric, submetric, value])
+
+
+def main():
+    from exe.ricbe.ric_mrstft_only import test_ricbe_ric_mrstft_only_config as config
+
+    print("# Loading...")
+    test(RicbeRicModelMrstftOnly(config.device), 
+         CheckpointsDirectory(config.checkpoints_directory), 
+         ValidationOrTestDataset(config.test_list, config.device).get_data_loader(32),
+         {
+             "mrstft": MrstftLossMetric.for_rir(config.device),
+             "l1": L1LossMetric(config.device),
+             "rt30": RirReverberationTimeMetrics(30, 16000, {
+                 "bias": BiasMetric(),
+                 "l1": L1LossMetric(config.device),
+                 "pearson": PearsonCorrelationMetric()
+             })
+         })
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/ricbe_models/ricbe_ric_model_mrstft_only.py
@@ -0,0 +1,89 @@
+from typing import Any, Literal, TypedDict
+from statictorch import Tensor0d, Tensor2d, Tensor3d
+import torch
+from torch.optim import AdamW  # pyright: ignore [reportPrivateImportUsage]
+
+from criterions.stft_losses.mrstft_loss import MrstftLoss
+from models.ricbe_models.networks.ricbe_ric_network import RicbeRicNetwork
+from trainers.trainable import Trainable
+
+
+class RicbeRicModelMrstftOnly(Trainable):
+    def __init__(self, device: torch.device) -> None:
+        super().__init__()
+        self.device = device
+
+        self.module = RicbeRicNetwork().to(device)
+        self.optimizer = AdamW(self.module.parameters(), 0.0001)
+
+        self.mrstft = MrstftLoss.for_rir(device)
+
+    class StateDict(TypedDict):
+        model: dict[str, Any]
+        optimizer: dict[str, Any]
+
+    def get_state(self) -> StateDict:
+        return {
+            "model": self.module.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+        }
+
+    def set_state(self, state: StateDict):
+        self.module.load_state_dict(state["model"])
+        self.optimizer.load_state_dict(state["optimizer"])
+
+    def _predict(self,
+                 reverb_batch: Tensor2d,
+                 speech_batch: Tensor2d) -> Tensor2d:
+        rir: Tensor3d = self.module(reverb_batch.unsqueeze(1), speech_batch.unsqueeze(1))
+        return Tensor2d(rir.squeeze(1))
+
+    def _calculate_losses(self, 
+                          actual: Tensor2d,
+                          predicted: Tensor2d) -> tuple[Tensor0d, dict[str, float]]:
+        mrstft: MrstftLoss.Return = self.mrstft(actual, predicted)
+
+        total: Tensor0d = mrstft.total()
+        return total, {
+            "loss_total": float(total),
+            "loss_mrstft_mag": float(mrstft.mag_loss),
+            "loss_mrstft_sc": float(mrstft.sc_loss)
+        }
+
+    def train_on(self, 
+                 reverb_batch: Tensor2d, 
+                 rir_batch: Tensor2d, 
+                 speech_batch: Tensor2d) -> dict[str, float]:
+        predicted: Tensor2d = self._predict(reverb_batch, speech_batch)
+
+        losses: dict[str, float]
+        loss_total: Tensor0d
+        loss_total, losses = self._calculate_losses(rir_batch, predicted)
+
+        self.optimizer.zero_grad()
+        loss_total.backward()
+        self.optimizer.step()
+
+        return losses
+
+    def evaluate_on(self, 
+                    reverb_batch: Tensor2d, 
+                    speech_batch: Tensor2d):
+        self.module.eval()
+        predicted: Tensor2d = self._predict(reverb_batch, speech_batch)
+        self.module.train()
+        return predicted
+    
+    def validate_on(self,
+                    reverb_batch: Tensor2d, 
+                    rir_batch: Tensor2d, 
+                    speech_batch: Tensor2d) -> tuple[float, dict[str, float]]:
+        self.module.eval()
+
+        predicted: Tensor2d = self._predict(reverb_batch, speech_batch)
+        losses: dict[str, float]
+        _, losses = self._calculate_losses(rir_batch, predicted)
+
+        self.module.train()
+        
+        return losses["loss_total"], losses
