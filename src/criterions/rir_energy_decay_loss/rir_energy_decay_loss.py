1d6c61e2e96d46ad58bb6cdc861c4a9faa559a80
yueyinqiu <yueyinqiu@outlook.com>
2025-03-03 01:35:18 +0800

    edecay

--- src/criterions/rir_energy_decay_loss/rir_energy_decay_loss.py
+++ src/criterions/rir_energy_decay_loss/rir_energy_decay_loss.py
@@ -1,10 +1,5 @@
-from typing import NamedTuple
 from statictorch import Tensor0d, Tensor2d
 import torch
-
-from audio_processors.rir_acoustic_features import RirAcousticFeatures
-from criterions.stft_losses.mrstft_loss_module import MrstftLossModule
-from criterions.stft_losses.stft_window import StftWindow
 
 
 class RirEnergyDecayLoss():
@@ -13,10 +8,10 @@ class RirEnergyDecayLoss():
         energy: torch.Tensor = rir ** 2
         energy = energy.flip(-1)
         energy = energy.cumsum(-1)
-        energy = energy.clamp_min(1e-6)
+        energy = energy.clamp_min(1e-8)
         return Tensor2d(energy.log10())
 
-    def __call__(self, actual: Tensor2d, predicted: Tensor2d):
+    def __call__(self, actual: Tensor2d, predicted: Tensor2d) -> Tensor0d:
         actual_energy: torch.Tensor = RirEnergyDecayLoss.energy_decay(actual)
         predicted_energy: torch.Tensor = RirEnergyDecayLoss.energy_decay(predicted)
-        return torch.nn.functional.l1_loss(predicted_energy, actual_energy)
+        return Tensor0d(torch.nn.functional.l1_loss(predicted_energy, actual_energy))
