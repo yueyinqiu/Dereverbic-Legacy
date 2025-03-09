fb8c3def4fd4743c243af9c81d84825c9518eb60
yueyinqiu <yueyinqiu@outlook.com>
2025-03-09 16:32:34 +0800

    redltest

--- src/criterions/rir_energy_decay_loss/rir_energy_decay_loss.py
+++ src/criterions/rir_energy_decay_loss/rir_energy_decay_loss.py
@@ -1,18 +1,48 @@
-from statictorch import Tensor0d, Tensor2d
+from pathlib import Path
+from typing import TypeVar
+import csdir
+import csfile
+import matplotlib
+import matplotlib.pyplot
+from statictorch import Tensor0d, Tensor1d, Tensor2d, Tensor3d, anify
 import torch
 
 
 class RirEnergyDecayLoss():
+    _TensorNd = TypeVar("_TensorNd",   # pylint: disable=un-declared-variable
+                        torch.Tensor, Tensor1d, Tensor2d, Tensor3d)
     @staticmethod
-    def _energy_decay(rir: Tensor2d) -> Tensor2d:
+    def _energy_decay(rir: _TensorNd) -> _TensorNd:
         energy: torch.Tensor = rir ** 2
         energy = energy.flip(-1)
         energy = energy.cumsum(-1)
         energy = energy.clamp_min(1e-8)
         energy = energy.log10()
-        return Tensor2d(energy)
+        return anify(energy)
 
     def __call__(self, actual: Tensor2d, predicted: Tensor2d) -> Tensor0d:
         actual_energy: torch.Tensor = RirEnergyDecayLoss._energy_decay(actual)
         predicted_energy: torch.Tensor = RirEnergyDecayLoss._energy_decay(predicted)
         return Tensor0d(torch.nn.functional.l1_loss(predicted_energy, actual_energy))
+    
+
+def _test():
+    outputs: Path = csdir.create_directory(".gitignored/RirEnergyDecayLoss")
+
+    t: torch.Tensor = torch.arange(15000) / 16000
+    rir: torch.Tensor = torch.exp(-t / 0.1) * torch.sin(10000 * t)
+    rir = torch.cat([torch.zeros([500]), rir, torch.zeros([500])], dim=0)
+    matplotlib.pyplot.xlim(0, 16000)
+    matplotlib.pyplot.plot(rir)
+    matplotlib.pyplot.savefig(outputs / "rir.png")
+    matplotlib.pyplot.clf()
+
+    ed: torch.Tensor = RirEnergyDecayLoss._energy_decay(rir)
+    matplotlib.pyplot.xlim(16000, 0)
+    matplotlib.pyplot.plot(ed)
+    matplotlib.pyplot.savefig(outputs / "ed.png")
+    matplotlib.pyplot.clf()
+
+
+if __name__ == "__main__":
+    _test()
